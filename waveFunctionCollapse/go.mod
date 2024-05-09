eb962fcdb3afa4b820d16386e7bd1f259a716088
mdarius31 <darius31murariu@gmail.com>
2024-05-09 20:07:35 +0300

    init wave function collapse

---
+++ waveFunctionCollapse/go.mod
@@ -0,0 +1,5 @@
+module waveFunctionCollapse
+
+go 1.22.2
+
+require fyne.io/fyne/v2 v2.4.5 // indirect
---
+++ waveFunctionCollapse/waveFunctionCollapse.go
@@ -0,0 +1,11 @@
+package main
+
+/* program inspiration:
+ * https://www.youtube.com/watch?v=rI_y2GAlQFM&list=PLB3OFCROxZ41eaR2Q4Ls27WjnzVoDLT6D
+ */
+
+import "fmt"
+
+func main() {
+	fmt.Println("Hello, World!")
+}
