5c363edb515508f9a16f3760aa6ba579cf4edf4c
mdarius31 <darius31murariu@gmail.com>
2024-05-11 14:01:04 +0300

    progress on wave function collapse

--- waveFunctionCollapse/waveFunctionCollapse.go
+++ waveFunctionCollapse/waveFunctionCollapse.go
@@ -5,14 +5,80 @@ package main
  */
 
 import (
+	"fyne.io/fyne/v2"
 	"fyne.io/fyne/v2/app"
+	"fyne.io/fyne/v2/canvas"
 	"fyne.io/fyne/v2/widget"
 )
 
-func main() {
-	a := app.New()
-	w := a.NewWindow("Hello, World!")
+type CellType int
 
-	w.SetContent(widget.NewLabel("Hello, World!"))
+const (
+	NOTHING CellType = iota
+	BLANK
+	LEFT
+	RIGHT
+	UP
+	DOWN
+)
+
+type CellData struct {
+	cellType CellType
+	row      int
+	col      int
+}
+
+func loadImage(s string) *canvas.Image {
+	i := canvas.NewImageFromFile(s)
+	i.FillMode = canvas.ImageFillOriginal
+	i.ScaleMode = canvas.ImageScalePixels
+
+	return i
+}
+
+func main() {
+
+	// nothing := loadImage("./tiles/demo/blank.png")
+	// blank := loadImage("./tiles/demo/blank.png")
+	// left := loadImage("./tiles/demo/left.png")
+	// right := loadImage("./tiles/demo/right.png")
+	// up := loadImage("./tiles/demo/up.png")
+	// down := loadImage("./tiles/demo/down.png")
+
+	rows := 10
+	cols := 10
+
+	var tableData [][]CellData
+
+	for r := 0; r < rows; r++ {
+
+		var row []CellData
+
+		for c := 0; c < cols; c++ {
+			row = append(row, CellData{cellType: NOTHING, row: r, col: c})
+		}
+
+		tableData = append(tableData, row)
+	}
+
+	a := app.New()
+	w := a.NewWindow("Wave Function Collapse")
+
+	list := widget.NewTable(
+		func() (int, int) {
+			return rows, cols
+		},
+
+		func() fyne.CanvasObject {
+			return loadImage("./tiles/demo/blank.png")
+		},
+
+		func(id widget.TableCellID, o fyne.CanvasObject) {
+
+		},
+	)
+
+	w.SetContent(list)
+
 	w.ShowAndRun()
 }
