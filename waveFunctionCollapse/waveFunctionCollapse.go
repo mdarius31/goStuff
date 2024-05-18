2a135937c3b95a2ca7aa7cf13d0ec50ae406c3e4
mdarius31 <darius31murariu@gmail.com>
2024-05-18 21:20:26 +0300

    wave function collapse progress(more rules needed)

--- waveFunctionCollapse/waveFunctionCollapse.go
+++ waveFunctionCollapse/waveFunctionCollapse.go
@@ -6,11 +6,26 @@ package main
 
 import (
 	"embed"
+	"math/rand/v2"
 
 	rl "github.com/gen2brain/raylib-go/raylib"
 )
 
 type CellType int
+
+type Cell struct {
+	collapsed bool
+	possible  []CellType
+}
+
+var allTypes = []CellType{BLANK,
+	LEFT,
+	RIGHT,
+	UP,
+	DOWN,
+}
+
+type WFCData [][]Cell
 
 const (
 	NOTHING CellType = iota
@@ -20,6 +35,35 @@ const (
 	UP
 	DOWN
 )
+
+var possibilies = map[CellType][]CellType{
+	NOTHING: allTypes,
+	BLANK:   allTypes,
+	LEFT: {
+		BLANK,
+		RIGHT,
+		UP,
+		DOWN,
+	},
+	RIGHT: {
+		BLANK,
+		LEFT,
+		UP,
+		DOWN,
+	},
+	UP: {
+		BLANK,
+		LEFT,
+		RIGHT,
+		DOWN,
+	},
+	DOWN: {
+		BLANK,
+		LEFT,
+		RIGHT,
+		UP,
+	},
+}
 
 //go:embed all:tiles/demo
 var tiles embed.FS
@@ -34,74 +78,147 @@ func loadTexture(s string) rl.Texture2D {
 
 }
 
-func main() {
-	rows := int32(10)
-	cols := int32(10)
-
-	width := int32(50)
-	height := int32(50)
-
-	var WFCData [][]CellType
-
+func resetWFCData(rows, cols int32) WFCData {
+	var WFCData WFCData
 	for r := int32(0); r < rows; r++ {
-		var row []CellType
+		var row []Cell
 
 		for c := int32(0); c < cols; c++ {
-			row = append(row, UP)
+			row = append(row, Cell{collapsed: false, possible: allTypes})
 		}
 		WFCData = append(WFCData, row)
 	}
 
+	return WFCData
+}
+
+func setCellType(WFCData WFCData, row, col int, cellType CellType) WFCData {
+	if WFCData[row][col].collapsed {
+		return WFCData
+	}
+
+	WFCData[row][col] = Cell{collapsed: true, possible: []CellType{cellType}}
+
+	rows := len(WFCData) - 1
+	cols := len(WFCData[0]) - 1
+	// DOWN
+	if row < rows {
+		if !WFCData[row+1][col].collapsed {
+
+			WFCData[row+1][col] = Cell{collapsed: false, possible: possibilies[cellType]}
+		}
+	}
+	// UP
+	if row >= rows {
+		if !WFCData[row-1][col].collapsed {
+
+			WFCData[row-1][col] = Cell{collapsed: false, possible: possibilies[cellType]}
+		}
+	}
+	// RIGHT
+	if col < cols {
+		if !WFCData[row][col+1].collapsed {
+
+			WFCData[row][col+1] = Cell{collapsed: false, possible: possibilies[cellType]}
+		}
+	}
+	// LEFT
+	if col >= cols {
+		if !WFCData[row][col-1].collapsed {
+
+			WFCData[row][col-1] = Cell{collapsed: false, possible: possibilies[cellType]}
+		}
+	}
+	return WFCData
+}
+
+func newRandomImg(rows, cols int32) WFCData {
+	var WFCData WFCData = resetWFCData(rows, cols)
+
+	// random pos
+	rr := rand.IntN(int(rows))
+	rc := rand.IntN(int(cols))
+
+	// making sure we dont draw a NOTHING
+	WFCData = setCellType(WFCData, rr, rc, CellType(rand.IntN(4)+1))
+
+	for r := 0; r < int(rows); r++ {
+		for c := 0; c < int(cols); c++ {
+			cell := &WFCData[r][c]
+			if !cell.collapsed {
+				WFCData = setCellType(WFCData, r, c, cell.possible[rand.IntN(len(cell.possible))])
+			}
+		}
+	}
+
+	return WFCData
+}
+
+const scale float32 = 0.25
+
+func main() {
+
+	rows := (100)
+	cols := (100)
+
+	width := (50)
+	height := (50)
+
+	var WFCData WFCData
+
 	rl.SetConfigFlags(rl.FlagWindowUndecorated)
 	rl.SetTraceLogLevel(rl.LogError)
 
-	rl.InitWindow(rows*height, cols*width, "Wave Function Collapse")
+	rl.InitWindow(int32(float32(rows*height)*scale), int32(float32(cols*width)*scale), "Wave Function Collapse")
+	closeWindow := false
 
-	blank := loadTexture("tiles/demo/blank.png")
-	left := loadTexture("tiles/demo/left.png")
-	right := loadTexture("tiles/demo/right.png")
-	up := loadTexture("tiles/demo/up.png")
-	down := loadTexture("tiles/demo/left.png")
+	tiles := map[CellType]rl.Texture2D{
+		BLANK: loadTexture("tiles/demo/blank.png"),
+		LEFT:  loadTexture("tiles/demo/left.png"),
+		RIGHT: loadTexture("tiles/demo/right.png"),
+		UP:    loadTexture("tiles/demo/up.png"),
+		DOWN:  loadTexture("tiles/demo/left.png"),
+	}
+
+	actions := map[uint]interface{}{
+		rl.KeyQ: func() {
+			closeWindow = true
+		},
+		rl.KeyR: func() {
+			WFCData = newRandomImg(int32(rows), int32(cols))
+		},
+	}
+	actions[rl.KeyR].(func())()
 
 	rl.SetTargetFPS(60)
 
-	for !rl.WindowShouldClose() {
+	for !rl.WindowShouldClose() && !closeWindow {
 		rl.BeginDrawing()
 
 		rl.ClearBackground(rl.RayWhite)
 
-		for r := int32(0); r < int32(len(WFCData)); r++ {
-			for c := int32(0); c < int32(len(WFCData[r])); c++ {
+		if action := actions[uint(rl.GetKeyPressed())]; action != nil {
+			action.(func())()
+		}
 
-				x := r * width
-				y := c * height
-				switch WFCData[r][c] {
-				case NOTHING:
-					rl.DrawTexture(blank, x, y, rl.White)
+		for r := (0); r < (len(WFCData)); r++ {
+			for c := (0); c < (len(WFCData[r])); c++ {
 
-				case BLANK:
-					rl.DrawTexture(blank, x, y, rl.White)
+				x := float32(c*height) * scale
+				y := float32(r*width) * scale
 
-				case DOWN:
-					rl.DrawTexture(down, x, y, rl.White)
+				if WFCData[r][c].collapsed {
+					possible := &WFCData[r][c].possible
+					r := rand.IntN(len(*possible))
 
-				case UP:
-					rl.DrawTexture(up, x, y, rl.White)
-
-				case RIGHT:
-					rl.DrawTexture(right, x, y, rl.White)
-
-				case LEFT:
-					rl.DrawTexture(left, x, y, rl.White)
-
-				default:
-					rl.DrawTexture(blank, x, y, rl.White)
-
+					rl.DrawTextureEx(tiles[(*possible)[r]], rl.NewVector2(x, y), 0.0, scale, rl.White)
 				}
+
 			}
 		}
 
 		rl.EndDrawing()
+
 	}
 
 	rl.CloseWindow()
